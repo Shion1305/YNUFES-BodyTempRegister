a69aaba663c2a521a1de358d7ec67be89ba40ba7
Shion Ichikawa <shion1305@gmail.com>
2022-05-30 20:10:30 +0900

    [CHANGE] modified GasResponse object

--- src/main/java/com/shion1305/ynufes/bodytemp2022/GasResponse.java
+++ src/main/java/com/shion1305/ynufes/bodytemp2022/GasResponse.java
@@ -1,7 +1,29 @@
 package com.shion1305.ynufes.bodytemp2022;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class GasResponse {
     public int code;
     public String message;
-    public GasResponse(){}
+    public String currentValue;
+
+    public GasResponse(@JsonProperty("code") int code,
+                       @JsonProperty("message") String message,
+                       @JsonProperty("currentValue") String currentValue) {
+        this.code = code;
+        this.message = message;
+        this.currentValue = currentValue;
+    }
+
+    @Override
+    public String toString() {
+        return "GasResponse{" +
+                "code=" + code +
+                ", message='" + message + '\'' +
+                ", currentValue='" + currentValue + '\'' +
+                '}';
+    }
 }
