af24216c55a525074341d521eea4862ee71998f6
Shion Ichikawa <shion1305@gmail.com>
2022-05-30 20:04:08 +0900

    [CHANGE] change GASCode.js

--- GASCode.js
+++ GASCode.js
@@ -7,30 +7,25 @@ var test1 = {
         "name": "市川詩恩"
     }
 };
-var test2 = {
-    "parameter": {
-        "type": "register",
-        "name": "市川詩恩",
-        "temp": "100.0"
-    }
-};
 
 function doGet(e) {
     return process(e);
 }
+
 const c = SpreadsheetApp.getActive().getSheetByName("編集現役");
+
 function process(e) {
-    if (!checkQualification(e)) return respond({ code: 400 });
+    if (!checkQualification(e)) return respond({code: 400});
     const indexN = checkNameExistance(e.parameter.name);
-    if (indexN === -1) return respond({ code: 404 });
-    if (e.parameter.type === "check") return respond({ code: 200 });
-    const d = new Date(new Date().toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' }));
+    if (indexN === -1) return respond({code: 404});
+    const d = new Date(new Date().toLocaleString('ja-JP', {timeZone: 'Asia/Tokyo'}));
     const indexD = searchRowForDate(d.getMonth() + 1, d.getDate());
-    if (indexD === -1) return respond({ code: 500 });
+    if (indexD === -1) return respond({code: 500});
     const cell = c.getRange(indexD, indexN + 1);
     const oldV = cell.getValue();
+    if (e.parameter.type === "check") return respond({currentValue: oldV, code: 200});
     cell.setValue(e.parameter.temp);
-    return respond({ code: 202, oldValue: oldV });
+    return respond({code: 202, oldValue: oldV});
 }
 
 function checkNameExistance(name) {
@@ -45,12 +40,13 @@ function searchRowForDate(month, date) {
     const dates = c.getRange(1, 1, c.getLastRow()).getValues();
     for (var i = 2; i <= dates.length; i++) {
         try {
-            const d = new Date(dates[i].toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' }));
+            const d = new Date(dates[i].toLocaleString('ja-JP', {timeZone: 'Asia/Tokyo'}));
             if (!d.getMonth() || !d.getDate()) continue;
             if (month == d.getMonth() && date == d.getDate()) return i + 1;
+        } catch (e) {
         }
-        catch (e) { }
-    } return -1;
+    }
+    return -1;
 }
 
 function checkQualification(e) {
@@ -61,26 +57,23 @@ function checkQualification(e) {
 }
 
 function respond(status) {
-    var response = {};
-    response.code = status.code;
     switch (status.code) {
         case 200:
-            response.message = "FOUND";
+            status.message = "FOUND";
             break;
         case 202:
-            response.message = "SUCCESS,REQUEST HAS BEEN PROCESSED"
+            status.message = "SUCCESS,REQUEST HAS BEEN PROCESSED"
             break;
         case 400:
-            response.message = "BAD REQUEST";
+            status.message = "BAD REQUEST";
             break;
         case 404:
-            response.message = "REQUESTED NAME NOT FOUND";
+            status.message = "REQUESTED NAME NOT FOUND";
             break;
         case 500:
-            response.message = "FAILED TO FIND TARGET DATE";
+            status.message = "FAILED TO FIND TARGET DATE";
             break;
     }
-    if (status.oldValue) respond.oldValue = status.oldValue;
-    return ContentService.createTextOutput(JSON.stringify(response))
+    return ContentService.createTextOutput(JSON.stringify(status))
         .setMimeType(ContentService.MimeType.JSON);
 }
