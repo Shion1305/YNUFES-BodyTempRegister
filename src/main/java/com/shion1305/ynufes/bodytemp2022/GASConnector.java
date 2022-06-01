c44ece1c9430640ddccb4a95d0b925a7c072e443
Shion Ichikawa <shion1305@gmail.com>
2022-06-02 03:14:31 +0900

    [CHORE] add copyright in all files

--- src/main/java/com/shion1305/ynufes/bodytemp2022/GASConnector.java
+++ src/main/java/com/shion1305/ynufes/bodytemp2022/GASConnector.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2022 Shion Ichikawa All Rights Reserved.
+ */
+
 package com.shion1305.ynufes.bodytemp2022;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
--- src/main/java/com/shion1305/ynufes/bodytemp2022/config/InstanceData.java
+++ src/main/java/com/shion1305/ynufes/bodytemp2022/config/InstanceData.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2022 Shion Ichikawa All Rights Reserved.
+ */
+
 package com.shion1305.ynufes.bodytemp2022.config;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
