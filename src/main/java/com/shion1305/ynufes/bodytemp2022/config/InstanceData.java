dcbe4a0714b4c92675e47aab0fc25c4788a0f418
Shion Ichikawa <shion1305@gmail.com>
2022-06-02 02:49:25 +0900

    [IMPLEMENT] Configuration management by Json data

---
+++ src/main/java/com/shion1305/ynufes/bodytemp2022/config/InstanceData.java
@@ -0,0 +1,17 @@
+package com.shion1305.ynufes.bodytemp2022.config;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class InstanceData {
+    public String name;
+    public String gasUrl;
+    public String lineToken;
+
+    public InstanceData(@JsonProperty("name") String name, @JsonProperty("GASUrl") String getGasUrl, @JsonProperty("LineMessagingToken") String token) {
+        this.name = name;
+        this.gasUrl = getGasUrl;
+        this.lineToken = token;
+    }
+}
---
+++ src/main/java/com/shion1305/ynufes/bodytemp2022/config/JsonConfigManager.java
@@ -0,0 +1,24 @@
+package com.shion1305.ynufes.bodytemp2022.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+
+public class JsonConfigManager {
+    static Logger logger = Logger.getLogger("JsonConfigManager");
+
+
+    public static InstanceData[] readJson() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        InstanceData[] data = mapper.readValue(new File(ConfigManager.getConfig(ConfigManager.ConfigProperty.BOT_CONFIG_JSON)), InstanceData[].class);
+        if (data == null) {
+            logger.info("JsonConfig\"" + ConfigManager.getConfig(ConfigManager.ConfigProperty.BOT_CONFIG_JSON) + "\" is not loaded properly");
+        } else {
+            logger.info("JsonConfigFound");
+        }
+        return data;
+    }
+}
